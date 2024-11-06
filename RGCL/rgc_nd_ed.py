b3ac4c95955b8127641d4a04a6717aeac2f5aefd
이지원 <giwon512@naver.com>
2024-11-06 16:23:56 +0900

    Update rgc_nd_ed.py

--- RGCL/rgc_nd_ed.py
+++ RGCL/rgc_nd_ed.py
@@ -51,11 +51,11 @@ def config():
     args.model_short_name = 'RGC-ND-ED'
 
     args.dataset_name = 'Digital_Music_5'
-    args.dataset_path = '/Digital_Music_5/Digital_Music_5.json'
+    args.dataset_path = './data/Digital_Music_5/Digital_Music_5.json'
     args.gcn_dropout = 0.7  # 0.7
     args.ed_alpha = 1.0  # 1.0
     args.nd_alpha = 0.3  # 0.3
-    args.device = 1
+    args.device = 0
     args.train_max_iter = 400
     args.train_classification = True
 
